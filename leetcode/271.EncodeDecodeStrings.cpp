df48e3f0d5de79ab0b9ab1652d383df8a25370e1
Brittany G <bmg890@gmail.com>
2022-10-24 14:24:26 -0700

    fix time complexity

--- leetcode/271.EncodeDecodeStrings.cpp
+++ leetcode/271.EncodeDecodeStrings.cpp
@@ -2,16 +2,19 @@ class Codec
 {
 public:
     // 271. Encode and Decode Strings: https://leetcode.com/problems/encode-and-decode-strings/
-    // Encode: Space Complexity: O(n); Time Complexity: O(n)
-    // Decode: Space Complexity: O(n); Time Complexity: O(n)
+    // Encode: Space Complexity: O(n*l); Time Complexity: O(n*l)
+    // Decode: Space Complexity: O(n*l); Time Complexity: O(n*l)
 
     // Encodes a list of strings to a single string.
     string encode(const vector<string> &strs)
     {
         string result;
         // Time complexity: number of words
-        // Space complexity: O(n)
+        // Space complexity: O(n*l)
         for (int i = 0; i < strs.size(); i++)
+            // for each word, store a combination of the
+            // number of characters, encode string, and the word itself
+            // this makes it easy to decode later
             result += to_string(strs[i].size()) + mEncodeStr + strs[i];
 
         return result;
@@ -23,20 +26,22 @@ public:
         vector<string> result;
         string word;
         int i = 0;
-        // Time complexity: number of words that were decoded
+        // Time complexity: O(n*l) number of words that were decoded and length of each word
         // Space complexity: O(n) for the result vector
         while (i < s.size())
         {
             string tmp;
             while (s[i] != mEncodeStr[0] && i < s.size())
             {
+                // extract the digits prior to the encoding string
                 tmp += s[i];
                 i++;
             }
 
-            int lengthOfWord = stoi(tmp);
+            // use the length of the word to identify indices of substring for word
+            int lengthOfWord = stoi(tmp); // O(k) -- number of digits of word
 
-            result.push_back(s.substr(i + mEncodeStr.size(), lengthOfWord));
+            result.push_back(s.substr(i + mEncodeStr.size(), lengthOfWord));  // O(l) length of word
             i += mEncodeStr.size() + lengthOfWord;
         }
 
