a1c98a6894ed1325f0bacaa618e9f5f182d0326b
Brittany G <bmg890@gmail.com>
2022-10-25 20:43:05 -0700

    fix

--- leetcode/121.BestTimeToBuyAndSellStock.cpp
+++ leetcode/121.BestTimeToBuyAndSellStock.cpp
@@ -9,14 +9,13 @@ public:
     {
         int buy = prices[0];
         int maxProfit = 0;
-        
+
         // Loop through each price values in the prices array
         // Store the minimum value of all inspected values in the buy variable
         // Find the max profit by subtracting the current price value 
         // from the smallest value that's been seen up to this point
         for (int i = 1; i < prices.size(); i++)
         {
-
             buy = min(buy, price);
             maxProfit = max(price - buy, maxProfit);
         }
